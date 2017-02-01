b625cec8ce918855c1f1b8a7ef3d5a6bdca263ed
MavenPOC <ashmita.kapoor@oracle.com>
2017-02-01 14:36:50 +0530

    Update SimpleTest.java

--- CounterWebApp/src/test/java/com/javacodegeeks/unit/SimpleTest.java
+++ CounterWebApp/src/test/java/com/javacodegeeks/unit/SimpleTest.java
@@ -15,6 +15,6 @@ public class SimpleTest {
 		SampleExample example = new SampleExample();
 		example.addInteger(10);
 		example.addInteger(100);
-		Assert.assertEquals(example.getSize(), System.getProperty("param"));
+		Assert.assertEquals(example.getSize(), Integer.parseInt(System.getProperty("param")));
 	}
 }
